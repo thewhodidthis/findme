0d479bed10aac394c70a2ed322ced6eb1795685c
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-07-08 09:19:11 +0200

    test: move one dir up

---
+++ test.js
@@ -0,0 +1,15 @@
+const test = require('tape')
+
+const config = require('./config')()
+const findme = require('./')(config)
+
+test('will report', (t) => {
+  t.plan(3)
+
+  findme((error, response, body) => {
+    t.error(error, 'Response errors none')
+    t.ok(body.content.length)
+    t.ok(body.content[0].msg.statusCode, 200)
+  })
+})
+
