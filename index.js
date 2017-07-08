0993accf3db6375509a2a4a59b20dfcbf57a8a5d
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-07-08 09:18:13 +0200

    module: cleanup, drop semicolons

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-'use strict';
+'use strict'
 
-var https = require('https');
+const https = require('https')
 
-const défauts = {
+const defaults = {
   headers: { 'Content-Type': 'application/json; charset=utf-8', Origin: 'https://www.icloud.com' },
   hostname: 'setup.icloud.com',
   method: 'POST',
   path: '/setup/ws/1/login',
-};
+}
 
-const send = (options, echo, data = '') => {
-  Object.assign(options.headers, { 'Content-Length': Buffer.byteLength(data) });
+const send = ({ request = defaults, callback = (() => {}), data = '' } = {}) => {
+  Object.assign(request.headers, { 'Content-Length': Buffer.byteLength(data) })
 
-  https.request(options)
-    .on('error', echo)
+  https.request(request)
+    .on('error', callback)
     .on('response', (response) => {
-      const body = [];
+      const body = []
 
       if (response.statusCode === 200) {
         response
           .on('data', (chunk) => {
-            body.push(chunk);
+            body.push(chunk)
           })
-          .on('end', () => echo(null, response, JSON.parse(Buffer.concat(body))));
+          .on('end', () => callback(null, response, JSON.parse(Buffer.concat(body))))
       } else {
-        echo(Error(`HTTP ${response.statusCode}`), response, null);
+        callback(Error(`HTTP ${response.statusCode}`), response, null)
       }
 
-      response.on('error', echo);
+      response.on('error', callback)
     })
-    .end(data);
-};
+    .end(data)
+}
 
 /**
  * Helps query find my iPhone service
@@ -41,59 +41,59 @@ const send = (options, echo, data = '') => {
  * const finder = findme({ apple_id: ***, password: *** });
  */
 const find = (appleId) => {
-  const id = Object.assign(appleId, { extended_login: true });
+  const request = Object.assign({}, defaults)
 
-  const session = { id: JSON.stringify(id), expires: Date() };
-  const options = Object.assign({}, défaut);
+  const id = Object.assign(appleId, { extended_login: true })
+  const login = { id: JSON.stringify(id), expires: Date() }
 
-  const login = reply => (error, response, body) => {
+  const pivot = callback => (error, response, body) => {
     if (error) {
-      reply(error);
+      callback(error)
 
-      return;
+      return
     }
 
-    const { findme } = body.webservices;
+    const { findme } = body.webservices
 
     // Break away if findme disabled
     if (findme.status !== 'active') {
-      reply(Error('findme service disabled'));
+      callback(Error('findme service disabled'))
 
-      return;
+      return
     }
 
     // Get cookie array
-    const cookie = response.headers['set-cookie'];
+    const cookie = response.headers['set-cookie']
 
     if (cookie) {
       // Set the expiry date based on this cookie entry
-      const webauth = cookie.filter(entry => entry.includes('X-APPLE-WEBAUTH-USER')).shift();
-      const expires = webauth.match(/Expires=(.*GMT+);/).pop();
+      const webauth = cookie.filter(entry => entry.includes('X-APPLE-WEBAUTH-USER')).shift()
+      const expires = webauth.match(/Expires=(.*GMT+);/).pop()
 
-      session.expires = Date(expires);
+      login.expires = Date(expires)
 
       // Cleanup cookie array and update headers
-      options.headers.Cookie = cookie.map(entry => entry.substr(0, entry.indexOf(';'))).join('; ');
+      request.headers.Cookie = cookie.map(entry => entry.substr(0, entry.indexOf(';'))).join('; ')
     }
 
     // Cleanup webservice url, update path, hostname
-    options.hostname = findme.url.replace(':443', '').replace('https://', '');
-    options.path = '/fmipservice/client/web/initClient';
+    request.hostname = findme.url.replace(':443', '').replace('https://', '')
+    request.path = '/fmipservice/client/web/initClient'
 
     // Make the call
-    send(options, reply);
-  };
+    send({ request, callback })
+  }
 
-  return (reply) => {
+  return (callback) => {
     // If session within limits
-    if (session.expires > Date()) {
+    if (login.expires > Date()) {
       // Go ahead
-      send(options, reply);
+      send({ request, callback })
     } else {
       // Login first
-      send(défauts, login(reply), session.id);
+      send({ callback: pivot(callback), data: login.id })
     }
-  };
-};
+  }
+}
 
-module.exports = find;
+module.exports = find
