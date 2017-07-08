5e73f60456a00cf90ecdc8ec3f37381c037e8164
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-07-08 09:19:31 +0200

    example: move one dir up

---
+++ example.js
@@ -0,0 +1,18 @@
+const config = require('./config')()
+const finder = require('./')(config)
+
+const logger = (error, response, body) => {
+  if (error) {
+    console.error(error)
+    process.exit(1)
+  } else {
+    body.content.forEach(console.log)
+  }
+}
+
+const findme = () => finder(logger)
+
+findme()
+
+// Try again to check cookie's been properly set
+setTimeout(findme, 30000)
