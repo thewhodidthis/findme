17f80b955b69f775e8a2c497778d39a39a50f849
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-08-08 11:26:33 +0200

    test: add check sans callback, fix per latest amends

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const test = require('tape')
 
 const config = require('./config')()
@@ -6,9 +8,14 @@ const findme = require('./')(config)
 test('will report', (t) => {
   t.plan(3)
 
-  findme((error, response, body) => {
+  findme((error, body) => {
     t.error(error, 'Response errors none')
     t.ok(body.content.length)
     t.ok(body.content[0].msg.statusCode, 200)
   })
+})
+
+test('will run sans callback', (t) => {
+  t.doesNotThrow(findme)
+  t.end()
 })
