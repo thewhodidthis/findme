0f36ff1020e6330aa436d8163d615f2b2bfcc178
Sotiri Bakagiannis <sotirios.bakagiannis@gmail.com>
2020-06-12 16:33:44 +0100

    example: fix for JSON parsing result, clean up

--- example.js
+++ example.js
@@ -4,20 +4,24 @@
 // '.npmrc' or 'package.json' config block, or
 // passed in as environment variables
 const {
-  PASSWORD, npm_config_password: password = PASSWORD,
-  APPLE_ID, npm_config_apple_id: apple_id = APPLE_ID
+  PASSWORD, npm_config_PASSWORD: password = PASSWORD,
+  APPLE_ID, npm_config_APPLE_ID: apple_id = APPLE_ID
 } = process.env
 
 // 1. Pass in credentials
-const finder = require('./')({ password, apple_id })
+const finder = require('./')({ apple_id, password })
 
 // 2. Stage response callback handler
-const findme = () => finder((error, { content = [] } = {}) => {
+const findme = () => finder((error, result) => {
   if (error) {
     console.error(error)
     process.exit(1)
   } else {
-    content.forEach(console.log)
+    const { content = [] } = JSON.parse(result)
+
+    content.forEach((data) => {
+      console.log(data)
+    })
   }
 })
 
