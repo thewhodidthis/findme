ce7c472799616844060c55f75acf9cc160aa66b0
Sotiri Bakagiannis <sotirios.bakagiannis@gmail.com>
2020-06-12 16:33:29 +0100

    test: fix for JSON parsing result, clean up

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 'use strict'
 
 // Using a local '.npmrc' is an easy way of sourcing private keys
-const { npm_config_password: password, npm_config_apple_id: apple_id } = process.env
-const findme = require('./')({ password, apple_id })
+const { npm_config_PASSWORD: password, npm_config_APPLE_ID: apple_id } = process.env
+const findme = require('./')({ apple_id, password })
 
 const { ok, equal, doesNotThrow } = require('tapeless')
 
@@ -10,10 +10,12 @@ doesNotThrow
   .describe('does not throw', 'will proceed sans callback')
   .test(findme, Error)
 
-findme((error, { content }) => {
+findme((error, result) => {
   equal
     .describe('response errors none', 'will report')
     .test(error, null)
+
+  const { content = [] } = JSON.parse(result)
 
   ok.test(content.length)
   equal.test(content[0].msg.statusCode, 200)
